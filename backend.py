58723918b821ad15b197ec12a0e1ecac5214f9b8
typar <typarris421@gmail.com>
2021-08-28 12:24:05 -0400

    backend queries

--- backend.py
+++ backend.py
@@ -28,6 +28,36 @@ def view():
     return rows
 
 
+def search(title="", author="", year="", isbn=""):
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * from book WHERE title=? OR author=? OR year=? OR isbn=?",
+                (title, author, year, isbn))
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+
+
+def delete(id):
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("DELETE FROM book WHERE id=? ", (id,))
+    conn.commit()
+    conn.close()
+
+
+def update(id, title, author, year, isbn):
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("UPDATE book SET title=?, author=?, year=?, isbn=? WHERE id=? ",
+                (title, author, year, isbn, id))
+    conn.commit()
+    conn.close()
+
+
 connect()
 insert("The sea", "John Tablet", 1918, 913123132)
 print(view())
+print(search(author="John Tablet"))
+# delete(1)
+update(1, "The earth", "John Smooth", 1917, 999999)
